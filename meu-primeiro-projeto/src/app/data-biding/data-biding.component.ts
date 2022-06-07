75ca99caab0215dbf2b9754d3486d5578c27998b
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-07 16:05:55 -0300

    Event Binding

--- meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -15,9 +15,19 @@ export class DataBidingComponent implements OnInit {
   public imgSrc: string = 'https://upload.wikimedia.org/wikipedia/pt/3/3d/Twitter_logo_2012.svg';
   public imgTitle: string = 'Property Binding';
 
+  public position: { x: number, y: number } = { x: 0, y: 0 };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMuve(valor: MouseEvent) {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
