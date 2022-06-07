15701d37ce74d1287c6ddadca5a80743ac0d34e8
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-07 17:48:09 -0300

    ngFor e ngSwitch

--- meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -10,6 +10,14 @@ export class DiretivasEstruturaisComponent implements OnInit {
   public condition: boolean = true;
   public conditionClick: boolean = true;
 
+  public list: Array<{ nome: string, idade: number }> = [
+    { nome: 'Lucas', idade: 20 },
+    { nome: 'Eduardo', idade: 21 },
+    { nome: 'Carvalho', idade: 22 }
+  ];
+
+  public nome: string = 'Lucas';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -21,7 +29,15 @@ export class DiretivasEstruturaisComponent implements OnInit {
   }
 
   public onClick() {
-    this.conditionClick = !this.conditionClick
+    this.conditionClick = !this.conditionClick;
+  }
+
+  public onClickAddList() {
+    this.list.push({ nome: 'Teste', idade: 55 });
+  }
+
+  public onClickEventList(event: number) {
+    this.list.splice(event, 1);
   }
 
 }
