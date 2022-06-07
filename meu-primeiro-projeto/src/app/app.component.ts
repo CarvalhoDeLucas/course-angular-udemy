3b8e491d0a84feaa906ce20ef51461222658b82c
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-07 15:45:47 -0300

    Interpolation

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,31 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-title *ngIf="destruir"></app-title>
-    <br>
-    <button (click)="desctruiComponent()">Destruir componente</button>
+    <app-data-biding></app-data-biding>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
 
-  public valor: number = 1;
-  public destruir: boolean = true;
-
   constructor() { }
 
-  public adicionar(): number {
-    return this.valor += 1;
-  }
-
-  public desctruiComponent() {
-    this.destruir = false;
-  }
-
   ngOnInit(): void {
-    setTimeout(() => {
-      console.log(1)
-    }, 5000);
   }
 
 }
