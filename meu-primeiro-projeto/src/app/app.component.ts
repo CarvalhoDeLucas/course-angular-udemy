54ccf643018c4ab205baf5c4f544840466caae72
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-08 09:24:11 -0300

    @Output()

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -10,14 +10,25 @@ import { Component, OnInit } from '@angular/core';
     </app-diretivas-atributos>
     -->
     
+    <!--
     <app-input [contador]="addValue"></app-input>
     <button (click)="add()"> Add </button>
+    -->
+
+    <ng-template [ngIf]="getDados">
+      <h1>{{getDados.nome}}</h1>
+      <h2>{{getDados.idade}}</h2>
+    </ng-template>
+
+    <app-output (enviarDados)="setDados($event)"></app-output>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
 
   public addValue: number = 0;
+
+  public getDados: { nome: string, idade: number } | undefined;
 
   constructor() { }
 
@@ -26,6 +37,10 @@ export class AppComponent implements OnInit {
 
   public add(): void {
     this.addValue++;
+  }
+
+  public setDados(event: { nome: string, idade: number }) {
+    this.getDados = event;
   }
 
 }
