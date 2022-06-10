3a89531ffc017aea7333f8273d07c6d06e041068
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-10 07:56:33 -0300

    Service - Injeção de Dependencia

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -15,12 +15,16 @@ import { Component, OnInit } from '@angular/core';
     <button (click)="add()"> Add </button>
     -->
 
+    <!--
     <ng-template [ngIf]="getDados">
       <h1>{{getDados.nome}}</h1>
       <h2>{{getDados.idade}}</h2>
     </ng-template>
 
     <app-output (enviarDados)="setDados($event)"></app-output>
+    -->
+
+    <app-food-list></app-food-list>
     <router-outlet></router-outlet>
   `
 })
---
+++ meu-primeiro-projeto/src/app/services/food-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+
+  private list: Array<string> = [
+    'X Bacon',
+    'Feijão',
+    'Ovo'
+  ];
+
+  constructor() { }
+
+  public foodList(): Array<string> {
+    return this.list;
+  }
+
+}
