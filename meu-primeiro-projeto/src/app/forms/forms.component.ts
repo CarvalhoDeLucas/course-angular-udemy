af6040b67369799e4459d73a6bedcf9a377054d1
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-27 22:03:26 -0300

    ngForm + Selected

--- meu-primeiro-projeto/src/app/forms/forms.component.ts
+++ meu-primeiro-projeto/src/app/forms/forms.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public listComidas: Array<{ comida: string, preco: string }> = [
+    { comida: 'X-Salada', preco: 'R$10,00' },
+    { comida: 'X-Bacon', preco: 'R$20,00' },
+    { comida: 'X-Calabresa', preco: 'R$30,00' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
