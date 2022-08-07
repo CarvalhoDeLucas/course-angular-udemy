c42af7fc0b0095dd62e321b1e079939bdbc6efbf
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-08-07 11:55:56 -0300

    Hands on 17

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -25,10 +25,10 @@ export class DetailsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon() {
+  public getPokemon() {
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGerPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGerPokemons(`${this.urlName}/${id}`);
