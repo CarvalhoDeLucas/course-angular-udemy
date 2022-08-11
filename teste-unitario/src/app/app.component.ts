7705a4b130c20b6bcad3d6a9e8d354687a0ed5f6
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-08-10 23:50:26 -0300

    Criando componente para teste

--- teste-unitario/src/app/app.component.ts
+++ teste-unitario/src/app/app.component.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<app-banking></app-banking>`
 })
-export class AppComponent {
-  title = 'teste-unitario';
-
-  public soma(val1: number, val2: number) {
-    return val1+val2;
-  }
-}
+export class AppComponent { }
