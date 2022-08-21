8a13cba4c4b1eb8a295aaa5ca56b09c22137bb78
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-08-21 13:50:47 -0300

    CanLoad

--- protecao-rotas/src/app/app-routing.module.ts
+++ protecao-rotas/src/app/app-routing.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 //Components Pages
-import { HomeComponent } from './shared/home/home.component';
-import { AccountComponent } from './shared/account/account.component';
+import { HomeComponent } from './shared/pages/home/home.component';
+import { AccountComponent } from './shared/pages/account/account.component';
 
 //Guards
 import { CanActiveGuard } from './shared/guards/can-active.guard';
 import { CanDeactiveGuard } from './shared/guards/can-deactive.guard';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-},
-{
-  path: 'account',
-  component: AccountComponent,
-  canActivate: [CanActiveGuard],
-  canDeactivate: [CanDeactiveGuard]
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [CanActiveGuard],
+    canDeactivate: [CanDeactiveGuard]
+  },
+  {
+    path: 'core',
+    loadChildren: () => import('./core/core.module').then((m) => m.CoreModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
