ed3d08818ebe64591ea22df024bab0a698d947d7
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-09-06 08:28:22 -0300

    Hands on 11 - Mechendo com LocalStorage

--- login/curso-de-angular-auth-front/src/app/core/services/auth.service.ts
+++ login/curso-de-angular-auth-front/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
@@ -8,17 +9,27 @@ import { catchError, map, Observable, throwError } from 'rxjs';
 export class AuthService {
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   public sing(payload: { email: string, password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map((data) => {
-        return console.log(data);
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
       }),
       catchError((err) => {
-        return throwError(() => err.error.message)
+        if (err.error.message) {
+          return throwError(() => err.error.message);
+        }
+
+        return throwError(() => "Inoperante, tente novamente mais tarde!");
       })
-    )
+    );
   }
 
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
 }
